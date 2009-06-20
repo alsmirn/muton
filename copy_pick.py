e7bb11a3aaed7786bef08f79da61f8a4ef4a7035
darkcreation <artemov.alexandr@gmail.com>
2009-06-20 22:03:18 +0400

    Fixed bugs with unscreend symbols

--- copy_pick.py
+++ copy_pick.py
@@ -12,7 +12,7 @@ class FileCopierBySign():
     _TAGS_TO_CLEAN = ('artist', 'album')
     _OBLIGATORY_TAGS = _TAGS_TO_CLEAN + ('year', )
     
-    _RESTR_SYMB = ('|', ':', '\\', '/', '?', '<', '>', '*', '"')
+    _RESTR_SYMB = ('|', ':', r'\\', '/', '\?', '<', '>', '\*', '"')
     _DEPR_PUNCT = (' ', '..', '...')
 
     def __init__(self, collection):
@@ -22,24 +22,25 @@ class FileCopierBySign():
         """Copy files to the selected folder with specified parameters
         """
         #Trying iterate scanned tags in collection
-        for current_file_path, scanned_tags in self.collection.items():
+        for curr_f_path, scanned_tags in self.collection.items():
             #Creates for each artist and album separate folder and copies
             #files to them
             try:
                 tmp_var = self._TAGS_TO_CLEAN[0]
             except TypeError:
-                print ("%r is not a valid audio file") % (current_file_path, )
+                print ("%r is not a valid audio file") % (curr_f_path, )
 
             if not scanned_tags[info_type].count(search_item):
                 # not found
                 continue
             else:
                 #Stripping restricted symbols in artist and album name
-                # @note: THIS OPERATION YOU CAN DO IN ONE REGULAR EXPR, THINCK!
                 for tag in self._TAGS_TO_CLEAN:
+                    #try:
                     for rs in self._RESTR_SYMB:
-                        scanned_tags[tag] = re.sub(r'[ ]?%s' % rs, '', scanned_tags[tag])
-
+                        scanned_tags[tag] = re.sub('%s' % rs, '', scanned_tags[tag])
+#                    except Exception, msg:
+#                        print Exception, msg
                     for wdp in self._DEPR_PUNCT:
                         scanned_tags[tag] = scanned_tags[tag].strip(wdp)
 
@@ -48,9 +49,10 @@ class FileCopierBySign():
                     #Making path for the extraction
                     
                     #@TODO: automatic build fmt parameter from _OBLIGATORY_TAGS
-                    fmt = "%(artist)s %(year)d %(album)s"
+                    #by now year would be a string instead of int
+                    fmt = "%(artist)s %(year)s %(album)s" 
                     
                     out_file_path = os.path.join(output_path, fmt.decode('utf-8') % scanned_tags)
                     dir_util.mkpath(out_file_path)
                     #Copying files
-                    file_util.copy_file(current_file_path, out_file_path, 'update = true')
+                    file_util.copy_file(curr_f_path, out_file_path, 'update = true')

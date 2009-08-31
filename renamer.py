84d4cdf95964fdbc67cb88ebe31b6ddde28909d2
alsmirn <alsmirn@gmail.com>
2009-08-31 19:15:21 +0400

    renamer module was cleaned

--- renamer.py
+++ renamer.py
@@ -22,41 +22,39 @@ class Renamer():
         self.make_sample(path, pattern)
 
         #Renaming file
-        new_name = os.path.join(os.path.split(path)[0], self.ren_sample + \
-        os.path.splitext(path)[1])
+        new_name = os.path.join(os.path.split(path)[0], 
+            self.ren_sample + os.path.splitext(path)[1])
         try:
             os.rename(path, new_name)
         except WindowsError:
             print 'The filename, directory name, or volume label syntax is incorrect', \
-            path
+                path
 
     def make_sample(self, path, pattern):
 
-        restr_symbols = '|', ':', '\\', '/', '?', '<', '>', '*', '"'
-        win_depr_punct = ' ', '..', '...'
-
         key = self.collection[path]
+        
+        restr_symbols = ('|', ':', '\\', '/', '?', '<', '>', '*', '"')
+        win_depr_punct = (' ', '..', '...')
+
+        tag_items = {}
+        tag_items_names = ('artist', 'album', 'album', 'year', 'genre', 'title')
+        
         #Making safe print of track number
-        if 'track' in key.keys() and re.search('\/', key['track']):
-            m = re.search('\/', key['track'])
-            track_n = key['track'][0:m.start()]
-        elif 'track' in key.keys():
-            track_n = key['track']
-        else: track_n = ''
-
-        #Adding 0 to track numbers without 0 - with 0 files are sorted better
-        if track_n != '' and len(track_n) <= 1:
-            track_n = '0' + track_n
-
-        tag_items = {'artist': key['artist'],
-            'album': key['album'],
-            'year': key['year'],
-            'genre': key['genre'],
-            'title': key['title'],
-            'track': track_n}
-
-        s = pattern.split('%')
+        track_number = str()
+        track_tag_name = 'track'
+        
+        if track_tag_name in key.keys():
+            track_number = "%02d" % key[track_tag_name].split('/')[0]
+        
+        tag_items[track_tag_name] = track_number
+        
+        #Filling tag_items from file
+        for item in tag_items_tuple:
+            tag_items[item] = key[item]
+        
         #Replacing list's items by tag info
+        s = pattern.split('%')
         for i in range(len(s)):
             try:
                 s[i] = tag_items[s[i]]
@@ -66,10 +64,8 @@ class Renamer():
 
         #Stripping restricted symbols
         for rs in restr_symbols:
-            if re.search(self.ren_sample, ' ' + rs):
-                self.ren_sample = self.ren_sample.replace(rs, '')
-            elif self.ren_sample.find(rs) != - 1:
+            if rs in self.ren_sample:
                 self.ren_sample = self.ren_sample.replace(rs, '')
 
         for wdp in win_depr_punct:
-            self.ren_sample = self.ren_sample.lstrip(wdp).rstrip(wdp)
+            self.ren_sample = self.ren_sample.strip(wdp)
